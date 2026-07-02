d44c3704571a76acfd074f0dcb172f9942cc82d5
Murilo <muriloaquinotrab@gmail.com>
2026-07-02 15:45:41 -0300

    Add HomeController with a home endpoint

---
+++ src/main/java/pet_link/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package pet_link.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "PetLink API Online";
+    }
+}

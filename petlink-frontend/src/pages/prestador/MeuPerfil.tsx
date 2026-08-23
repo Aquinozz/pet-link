48c0bbda1b0786ed45a14084ddfa6dc2a560273e
Aquinozz <muriloaquinotrab@gmail.com>
2026-08-23 14:36:46 -0300

    design: banner do prestador em 4:1 (proporção LinkedIn)

--- petlink-frontend/src/pages/prestador/MeuPerfil.tsx
+++ petlink-frontend/src/pages/prestador/MeuPerfil.tsx
@@ -273,7 +273,7 @@ await prestadorService.removerBanner()
           imageSrc={cropBanner}
           onCancel={() => setCropBanner(null)}
           onConfirm={handleUploadBanner}
-          aspectRatio={16 / 5}
+          aspectRatio={4}
         />
       )}
     </DashboardLayout>

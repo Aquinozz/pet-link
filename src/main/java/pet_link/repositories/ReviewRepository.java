505a1366a5d8c7a3ffe97567ff41cf72474ddffa
Aquinozz <238143133+Aquinozz@users.noreply.github.com>
2026-09-03 18:21:46 -0300

    Perf: Corrigindo N+1 de consultas

--- src/main/java/pet_link/repositories/ReviewRepository.java
+++ src/main/java/pet_link/repositories/ReviewRepository.java
@@ -1,5 +1,6 @@
 package pet_link.repositories;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pet_link.models.ReviewModel;
 
@@ -7,11 +8,18 @@ import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<ReviewModel, Long> {
 
+    @EntityGraph(attributePaths = {"tutor", "prestador", "agendamento"})
     List<ReviewModel> findByPrestadorId(Long prestadorId);
 
+    @EntityGraph(attributePaths = {"tutor", "prestador", "agendamento"})
     List<ReviewModel> findByTutor_Id(Long tutorId);
 
+    @EntityGraph(attributePaths = {"tutor", "prestador", "agendamento"})
     List<ReviewModel> findByPrestador_User_Id(Long userId);
+
+    @Override
+    @EntityGraph(attributePaths = {"tutor", "prestador", "agendamento"})
+    List<ReviewModel> findAll();
 
     boolean existsByAgendamento_Id(Long agendamentoId);
 }
